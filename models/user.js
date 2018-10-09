bb43b16fbf8640872e5c06f25e4c97fa4bd9a05d
maheshika <maheshiweera11@gmail.com>
2018-10-09 18:32:47 +0530

    profile details added

--- models/user.js
+++ models/user.js
@@ -14,13 +14,13 @@ db.connect((err)=>{
 
 
 module.exports.AllUsers = function(callback){
-    let dbquery = 'SELECT hairstylist_profile.*,skill.*,address.* FROM hairstylist_profile JOIN has_skill ON hairstylist_profile.Hairstylist_profile_id=has_skill.Has_skill_id JOIN skill ON has_skill.skill_id=skill.skill_id JOIN address ON hairstylist_profile.Hairstylist_profile_id=address.Hairstylist_profile_id';
+    let dbquery = "SELECT hairstylist_profile.*,skill.*,address.* FROM hairstylist_profile JOIN has_skill ON hairstylist_profile.Hairstylist_profile_id=has_skill.Has_skill_id JOIN skill ON has_skill.skill_id=skill.skill_id JOIN address ON hairstylist_profile.Hairstylist_profile_id=address.Hairstylist_profile_id";
     console.log('inside model');
     db.query(dbquery,callback);
 }
 
 module.exports.Profile = function(id,callback){
-    let dbquery = "SELECT hairstylist_profile.*,skill.*,address.*  FROM hairstylist_profile JOIN has_skill ON hairstylist_profile.Hairstylist_profile_id=has_skill.Has_skill_id JOIN skill ON has_skill.skill_id=skill.skill_id JOIN address ON hairstylist_profile.Hairstylist_profile_id=address.Hairstylist_profile_id WHERE hairstylist_profile.Hairstylist_profile_id = '"+id+"'";
+    let dbquery = "SELECT skill.Skill,hairstylist_profile.* FROM skill,has_skill,hairstylist_profile where  has_skill.Hairstylist_profile_id=hairstylist_profile.Hairstylist_profile_id=1 and skill.Skill_id=has_skill.Skill_id" ;
     db.query(dbquery,callback);
 }
 
