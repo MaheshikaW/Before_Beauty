d4de73e4a1aff3e6413b6285b46fc2be62c3dcb9
maheshika <maheshiweera11@gmail.com>
2018-10-09 14:44:55 +0530

    Profile data viewed

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ module.exports.AllUsers = function(callback){
 }
 
 module.exports.Profile = function(id,callback){
-    let dbquery = 'SELECT * FROM hairstylist_profile ';
+    let dbquery = "SELECT hairstylist_profile.*,skill.*,address.*  FROM hairstylist_profile JOIN has_skill ON hairstylist_profile.Hairstylist_profile_id=has_skill.Has_skill_id JOIN skill ON has_skill.skill_id=skill.skill_id JOIN address ON hairstylist_profile.Hairstylist_profile_id=address.Hairstylist_profile_id WHERE hairstylist_profile.Hairstylist_profile_id = '"+id+"'";
     db.query(dbquery,callback);
 }
 
@@ -30,6 +30,6 @@ module.exports.AllSkills = function(callback){
     db.query(dbquery,callback);
 }
 module.exports.UsersBySkill = function(skill,callback){
-    let dbquery = 'SELECT * FROM skill';
+    let dbquery = "SELECT * FROM skill WHERE Skill= '"+skill+"'";;
     db.query(dbquery,callback);
 }
